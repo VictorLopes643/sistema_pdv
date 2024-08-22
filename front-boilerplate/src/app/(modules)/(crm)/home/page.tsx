205f0be47384af3f311fdfb16c99598de7c6b1ba
victor lopes <victorlopes560@gmail.com>
2024-08-22 20:50:01 -0300

    feat

--- front-boilerplate/src/app/(modules)/(crm)/home/page.tsx
+++ front-boilerplate/src/app/(modules)/(crm)/home/page.tsx
@@ -1,26 +1,39 @@
 import style from "@/styles/modules/crm/home.module.scss";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Business management home page"
 };
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+  console.log("session", session);
+
   return (
-    <div className={style.container}>
-      <h1>Hey</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium in
-        modi fugit, ab corrupti doloremque distinctio. Ut similique maiores
-        nihil quia expedita soluta nesciunt et quas recusandae, accusamus,
-        adipisci impedit! Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Praesentium in modi fugit, ab corrupti doloremque distinctio. Ut
-        similique maiores nihil quia expedita soluta nesciunt et quas
-        recusandae, accusamus, adipisci impedit!accusamus, adipisci
-        impedit!adipisci impedit!adipisci impedit!adipisci impedit!adipisci
-        impedit!adipisci impedit!adipisci impedit!adipisci impedit!
-      </p>
-    </div>
+    <>
+    getServerSession Result
+    {session?.user?.name ? (
+      <div>{session?.user?.name}</div>
+    ) : (
+      <div>Not logged in</div>
+    )}
+  </>
+    // <div className={style.container}>
+    //   <h1>Hey</h1>
+    //   <p>
+    //     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium in
+    //     modi fugit, ab corrupti doloremque distinctio. Ut similique maiores
+    //     nihil quia expedita soluta nesciunt et quas recusandae, accusamus,
+    //     adipisci impedit! Lorem ipsum dolor sit amet consectetur, adipisicing
+    //     elit. Praesentium in modi fugit, ab corrupti doloremque distinctio. Ut
+    //     similique maiores nihil quia expedita soluta nesciunt et quas
+    //     recusandae, accusamus, adipisci impedit!accusamus, adipisci
+    //     impedit!adipisci impedit!adipisci impedit!adipisci impedit!adipisci
+    //     impedit!adipisci impedit!adipisci impedit!adipisci impedit!
+    //   </p>
+    // </div>
+
   );
 };
--- front-boilerplate/src/app/api/auth/[...nextauth]/route.ts
+++ front-boilerplate/src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ export const authOptions = {
     providers: [
         KeycloakProvider({
           clientId: process.env.KEYCLOAK_ID ?? "nextJs",
-          clientSecret: process.env.KEYCLOAK_SECRET ?? "wClz8XDtiTwba37pZgstM9gGTM06iyIO",
-          issuer: process.env.KEYCLOAK_ISSUER ?? "http://host.docker.internal:8080/auth/realms/saas",
+          clientSecret: process.env.KEYCLOAK_SECRET ?? "cX1B1al9tP4zSLHuWr3wO5FLNwmhKa9k",
+          issuer: process.env.KEYCLOAK_ISSUER ?? "http://host.docker.internal:8081/auth/realms/saas",
         })
     ]
 };
